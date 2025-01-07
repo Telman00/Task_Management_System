01474d9c6cec03edc41ce2f3b15f7bcca2ff40fe
Telman00 <telmanabidov00@gmail.com>
2025-01-07 13:10:29 +0400

    Add model, migration, views

--- resources/views/front/partials/sidebar.blade.php
+++ resources/views/front/partials/sidebar.blade.php
@@ -2,33 +2,33 @@
     <h5>Task Management</h5>
     <ul class="nav flex-column">
       <li class="nav-item">
-        <a class="nav-link" id="dashboard-link" href="#">
+        <a class="nav-link" id="dashboard-link" href="{{ route('home') }}">
           <i class="bi bi-speedometer2 text-primary"></i> Dashboard
         </a>
       </li>
       
       <li class="nav-item">
-        <a class="nav-link" href="#">
+        <a class="nav-link" href="{{ route('tasks.index') }}">
           <i class="bi bi-list-task text-success"></i> Tasks
         </a>
       </li>
     </ul>
     <ul class="nav flex-column">
       <li class="nav-item">
-        <a class="nav-link" href="#">
+        <a class="nav-link" href="{{ route('company.index') }}">
           <i class="bi bi-building text-warning"></i> Company
         </a>
       </li>
       
       <li class="nav-item">
-        <a class="nav-link" href="#">
+        <a class="nav-link" href="{{ route('projects.index') }}">
           <i class="bi bi-kanban text-info"></i> Projects
         </a>
       </li>
     </ul>
     <ul class="nav flex-column">
       <li class="nav-item">
-        <a class="nav-link" href="#">
+        <a class="nav-link" href="{{ route('calendar.index') }}">
           <i class="bi bi-calendar text-primary"></i> Calendar
         </a>
       </li>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Front\HomeController;
+use App\Http\Controllers\Front\TaskController;
+use App\Http\Controllers\Front\ProjectController;
+use App\Http\Controllers\Front\CompanyController;
+use App\Http\Controllers\Front\CalendarController;
+use App\Http\Controllers\Admin\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +18,16 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
+Route::get('/company', [CompanyController::class, 'index'])->name('company.index');
+Route::get('/calendar', [CalendarController::class, 'index'])->name('calendar.index');
 
-Route::get('/', function () {
-    return view('front.index');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/admin', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
 });
+
